292093940502af7f5c76e2b94be4ba3bca736d3b
danteWebCoder <danteonecoder@gmail.com>
2026-07-06 18:50:56 +0200

    feat: FRAMEWORK STYLES ADD neon bold

--- framework/dependencies/dinamicStyles/neon.js
+++ framework/dependencies/dinamicStyles/neon.js
@@ -30,3 +30,39 @@ light.info = () => {
         } 
     }`)
 }
+
+export const bold = ({
+    config = null,
+}) => {
+    if (config) {
+        config.element = config.element || null
+        config.color1 = config.color1 || null
+        config.color2 = config.color2 || null
+
+        if (!config.element || !config.color1 || !config.color2) {
+            bold.info()
+            return null
+        }
+
+        config.element.style.color = config.color1
+        config.element.style.textShadow = `
+        0 0 2px ${config.color1},
+        0 0 4px ${config.color1}, 
+        0 0 12px ${config.color2}, 
+        0 0 30px ${config.color2},
+        0 0 40px ${config.color2},
+        0 0 50px ${config.color2},
+        0 0 60px ${config.color2}`;
+        return true
+    }
+}
+
+bold.info = () => {
+    console.info(`NEONBOLD { 
+        config: { 
+            element: dom's element, 
+            color1: font's color, 
+            color2: shadow color 
+        } 
+    }`)
+}
